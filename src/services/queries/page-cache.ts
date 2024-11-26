c3b0bdcbc4851c1a1dae54bb0cff357bc07b53cb
Nick Dunn <nick.dunn@flosports.tv>
2024-11-26 13:19:07 -0600

    feat: add support for getting/setting cached pages

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,3 +1,10 @@
+const CachableRoutes: ReadonlyArray<string> = [
+	'/about',
+	'/privacy',
+	'/auth/signin',
+	'/auth/signup',
+]
+
 export const getCachedPage = (route: string) => {};
 
 export const setCachedPage = (route: string, page: string) => {};
