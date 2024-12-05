1d7fba2dff5ad7fec7b5b55657d7bf2a8d3e1582
Nick Dunn <nick.dunn@flosports.tv>
2024-12-05 17:30:13 -0600

    feat(items): implement getItem() method

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -1,16 +1,25 @@
 import type { CreateItemAttrs } from '$services/types'
-import * as ItemSerializers from './serialize'
+import { serialize } from './serialize'
+import { deserialize } from '$services/queries/items/deserialize'
 import { genId } from '$services/utils'
 import { client } from '$services/redis'
 import { itemsKey } from '$services/keys'
 
-export const getItem = async (id: string) => {}
+export const getItem = async (id: string) => {
+	const item = await client.hGetAll(itemsKey(id))
+
+	if (Object.keys(item).length === 0) {
+		return null
+	}
+
+	return deserialize(id, item)
+}
 
 export const getItems = async (ids: string[]) => {}
 
 export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
 	const id = genId()
-	const serialized = ItemSerializers.serialize(attrs)
+	const serialized = serialize(attrs)
 
 	await client.hSet(
 		itemsKey(id),
