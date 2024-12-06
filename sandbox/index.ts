ab690569c9153394accc0c495c6832ad1db0ce30
Nick Dunn <nick.dunn@flosports.tv>
2024-12-06 11:52:42 -0600

    chore: sandbox changes for pipelining

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,27 @@
-import 'dotenv/config';
-import { client } from '../src/services/redis';
+import 'dotenv/config'
+import { client } from '../src/services/redis'
 
-const run = async () => {};
-run();
+const run = async () => {
+	await client.hSet('car1', {
+		color: 'red',
+		year: 2001,
+	})
+	await client.hSet('car2', {
+		color: 'blue',
+		year: 2010,
+	})
+	await client.hSet('car3', {
+		color: 'yellow',
+		year: 2022,
+	})
+
+	const commands = [1,2,3].map(id => {
+		return client.hGetAll(`car${id}`)
+	})
+
+	const results = await Promise.all(commands)
+
+	console.log(results)
+}
+
+run()
