2e30a26cb62f0044f3576a16532959b6cd6c422e
Nick Dunn <nick.dunn@flosports.tv>
2024-12-09 14:03:35 -0600

    feat(likes): implement likedItems() function

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,11 +1,18 @@
 import { client } from '$services/redis'
 import { itemsKey, userLikesKey } from '$services/keys'
+import * as ItemsQueries from '$services/queries/items'
 
 export const userLikesItem = async (itemId: string, userId: string) => {
 	return client.sIsMember(userLikesKey(userId), itemId)
 }
 
-export const likedItems = async (userId: string) => {}
+export const likedItems = async (userId: string) => {
+	// Fetch all item ids that the user likes
+	const itemIds = await client.sMembers(userLikesKey(userId))
+	
+	// Return array of the actual Item objects
+	return ItemsQueries.getItems(itemIds)
+}
 
 export const likeItem = async (itemId: string, userId: string) => {
 	const inserted = await client.sAdd(userLikesKey(userId), itemId)
