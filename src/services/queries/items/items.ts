e4bbde79c71a6e1462c5b2f3392db16a9783347f
Nick Dunn <nick.dunn@flosports.tv>
2024-12-09 11:30:36 -0600

    feat(items): implement getItems() method

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -15,7 +15,18 @@ export const getItem = async (id: string) => {
 	return deserialize(id, item)
 }
 
-export const getItems = async (ids: string[]) => {}
+export const getItems = async (ids: string[]) => {
+	const commands = ids.map(id => client.hGetAll(itemsKey(id)))
+
+	const items = await Promise.all(commands)
+
+	return items.map((item, i) => {
+		if (Object.keys(item).length === 0) {
+			return null
+		}
+		return deserialize(ids[i], item)
+	})
+}
 
 export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
 	const id = genId()
