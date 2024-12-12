a0302ed4b7c8d45814062cbdef4898f715ec0749
Nick Dunn <nick.dunn@flosports.tv>
2024-12-11 20:37:42 -0600

    feat(auth): introduce signup functional via getUserByUsername()

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,9 +1,19 @@
 import type { CreateUserAttrs } from '$services/types'
 import { genId } from '$services/utils'
 import { client } from '$services/redis'
-import { usernamesUniqueKey, usersKey } from '$services/keys';
+import { usernamesKey, usernamesUniqueKey, usersKey } from '$services/keys';
 
-export const getUserByUsername = async (username: string) => {}
+export const getUserByUsername = async (username: string) => {
+	const decimalId = await client.zScore(usernamesKey(), username)
+	if (!decimalId) {
+		throw new Error('User does not exist!')
+	}
+
+	// decimalId is a number, but we need to convert it back to a hex string
+	const userId = decimalId.toString(16)
+
+	return getUserById(userId)
+}
 
 export const getUserById = async (id: string) => {
 	const user = await client.hGetAll(usersKey(id))
@@ -13,6 +23,7 @@ export const getUserById = async (id: string) => {
 
 export const createUser = async (attrs: CreateUserAttrs) => {
 	// See if the username is already taken
+	// TODO: Check the sorted set instead of the set, and remove the set
 	const usernameExists = await client.sIsMember(usernamesUniqueKey(), attrs.username)
 	if (usernameExists) {
 		throw new Error('Username already taken!')
@@ -20,12 +31,16 @@ export const createUser = async (attrs: CreateUserAttrs) => {
 
 	const userId = genId()
 
-	await client.hSet(
-		usersKey(userId),
-		serialize(attrs)
-	)
+	await client.hSet(usersKey(userId), serialize(attrs))
+
 	// Add the username to the set of unique usernames
+	// TODO: remove this, because we're already adding the username to the sorted set
 	await client.sAdd(usernamesUniqueKey(), attrs.username)
+	// Also add the username + userId pair to a sorted set
+	await client.zAdd(usernamesKey(), {
+		value: attrs.username,
+		score: parseInt(userId, 16), // <-- Convert userId hex string to a number
+	})
 
 	return userId
 }
