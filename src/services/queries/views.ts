6260ac099c4d59a88e85915180df1737ec506ee7
Nick Dunn <nick.dunn@flosports.tv>
2024-12-12 11:55:29 -0600

    feat(views): implement incrementView() function

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,1 +1,14 @@
-export const incrementView = async (itemId: string, userId: string) => {};
+import { client } from '$services/redis';
+import { itemsByViewsKey, itemsKey } from '$services/keys';
+
+/**
+ * Increment view count for an item in BOTH the items hash and the items:views sorted set.
+ *
+ * TODO: Prevent same user from incrementing view count multiple times.
+ */
+export const incrementView = async (itemId: string, userId: string) => {
+	return Promise.all([
+		client.hIncrBy(itemsKey(itemId), 'views', 1),
+		client.zIncrBy(itemsByViewsKey(), 1, itemId),
+	])
+}
