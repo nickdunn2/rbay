8031f1c7a41e5bdc9ce67c8d5370cbe9ee3133ba
Nick Dunn <nick.dunn@flosports.tv>
2024-12-14 14:59:32 -0600

    feat(items): implement itemsByEndingTime() for Ending Soonest

--- src/services/queries/items/by-ending-time.ts
+++ src/services/queries/items/by-ending-time.ts
@@ -1,5 +1,28 @@
+import { client } from '$services/redis';
+import { itemsByEndingAtKey, itemsKey } from '$services/keys';
+import { deserialize } from '$services/queries/items/deserialize';
+import type { Item } from '$services/types';
+
 export const itemsByEndingTime = async (
 	order: 'DESC' | 'ASC' = 'DESC',
 	offset = 0,
 	count = 10
-) => {};
+): Promise<Item[]> => {
+	const ids: string[] = await client.zRange(
+		itemsByEndingAtKey(),
+		Date.now(),
+		'+inf',
+		{
+			BY: 'SCORE',
+			LIMIT: {
+				offset,
+				count,
+			}
+		}
+	)
+
+	const commands = ids.map(id => client.hGetAll(itemsKey(id)))
+	const items = await Promise.all(commands)
+
+	return items.map((item, i) => deserialize(ids[i], item))
+}
