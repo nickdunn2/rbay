d62e52070442e4e4c81684dad5ac5fd25de5c82e
Nick Dunn <nick.dunn@flosports.tv>
2024-12-16 14:06:47 -0600

    feat(items): implement itemsByViews() using SORT

--- src/services/queries/items/by-views.ts
+++ src/services/queries/items/by-views.ts
@@ -1,1 +1,39 @@
-export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {};
+import { client } from '$services/redis';
+import { itemsByViewsKey, itemsKey } from '$services/keys';
+import { deserialize } from '$services/queries/items/deserialize';
+
+export const itemsByViews = async (
+	order: 'DESC' | 'ASC' = 'DESC',
+	offset = 0,
+	count = 10
+) => {
+	let results: any = await client.sort(
+		itemsByViewsKey(),
+		{
+			GET: [
+				'#',
+				`${itemsKey('*')}->name`,
+				`${itemsKey('*')}->views`,
+				`${itemsKey('*')}->endingAt`,
+				`${itemsKey('*')}->imageUrl`,
+				`${itemsKey('*')}->price`,
+			],
+			BY: 'nosort', // data is already sorted in the sorted set
+			DIRECTION: order,
+			LIMIT: {
+				offset,
+				count,
+			},
+		}
+	)
+
+	const items = []
+	while (results.length > 0) {
+		const [id, name, views, endingAt, imageUrl, price, ...rest] = results
+		const item = deserialize(id, { name, views, endingAt, imageUrl, price })
+		items.push(item)
+		results = rest
+	}
+
+	return items
+}
