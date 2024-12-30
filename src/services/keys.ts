aa265ad8f4e641b399ee9c46c6dcaf0e2855c01e
Nick Dunn <nick.dunn@FV14P6700K.attlocal.net>
2024-12-30 14:38:23 -0600

    fix(views): enforce view count uniqueness using HyperLogLog

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -3,6 +3,7 @@ export const sessionsKey = (sessionId: string) => `sessions#${sessionId}`
 
 // Items
 export const itemsKey = (itemId: string) => `items#${itemId}`
+export const itemsViewsKey = (itemId: string) => `items:views#${itemId}`
 export const itemsByViewsKey = () => 'items:views'
 export const itemsByEndingAtKey = () => 'items:endingAt'
 
--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,5 +1,5 @@
 import { client } from '$services/redis';
-import { itemsByViewsKey, itemsKey } from '$services/keys';
+import { itemsByViewsKey, itemsKey, itemsViewsKey } from '$services/keys';
 
 /**
  * Increment view count for an item in BOTH the items hash and the items:views sorted set.
@@ -7,8 +7,12 @@ import { itemsByViewsKey, itemsKey } from '$services/keys';
  * TODO: Prevent same user from incrementing view count multiple times.
  */
 export const incrementView = async (itemId: string, userId: string) => {
-	return Promise.all([
-		client.hIncrBy(itemsKey(itemId), 'views', 1),
-		client.zIncrBy(itemsByViewsKey(), 1, itemId),
-	])
+	const inserted = await client.pfAdd(itemsViewsKey(itemId), userId)
+
+	if (inserted) {
+		return Promise.all([
+			client.hIncrBy(itemsKey(itemId), 'views', 1),
+			client.zIncrBy(itemsByViewsKey(), 1, itemId),
+		])
+	}
 }
